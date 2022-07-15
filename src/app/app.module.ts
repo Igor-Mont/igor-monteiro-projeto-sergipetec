4d0b11e19ffc32b66c661e157ec51aaf6e7faa10
Igor Monteiro <igornathandev@gmail.com>
2022-07-14 22:58:51 -0300

    feat: add show users table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShowUsersComponent } from './shared/components/show-users/show-users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ShowUsersComponent,
   ],
   imports: [
     BrowserModule
